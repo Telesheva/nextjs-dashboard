61acd9f018d8a741e553788562172214614b3522
Iryna Telesheva <iryna.telesheva@intellecteu.com>
2025-10-16 17:52:09 +0200

    Add Invoices page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Only available in canary version of Next.js
+  // experimental: {
+  //   cacheComponents: true,
+  // },
 };
 
 export default nextConfig;
